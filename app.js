5a631090fdf76b6a28e704ffed21dc15edd234be
VictorLastre <117042793+VictorLastre@users.noreply.github.com>
2025-01-20 17:53:37 -0300

    Agregue: Limpiar input, verificar nombre vacio

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
 let amigos = [];
+
+function agregarAmigo() {
+  let nombre = document.getElementById("amigo").value;
+
+  if (!nombre) {
+    alert("Por favor ingrese un nombre..");
+    return;
+  } else {
+    limpiarInput();
+    amigos.push(nombre);
+    console.log(amigos);
+  }
+}
+
+function limpiarInput() {
+  document.querySelector("#amigo").value = "";
+}
+
+function setearTexto(elemento, texto) {
+  let elementoHtml = document.querySelector(elemento);
+  elemento.innerHTML = texto;
+  return;
+}
